59f4f93645710c8976862f05c8805cdaa616968b
Fan Jiang <i@fanjiang.me>
2023-11-20 12:59:10 -0500

    Add Linux support

--- esp_serial_find/cli.py
+++ esp_serial_find/cli.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import sys
 import plistlib
 from typing import Dict, List, Tuple
 
@@ -10,6 +11,42 @@ from .esp_serial_find import find_serial_and_path, find_subtree_paths
 
 logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
+
+
+def find_usb_devices(vendor_name):
+    devices = []
+    base_path = "/sys/bus/usb/devices/"
+    for device in os.listdir(base_path):
+        try:
+            manufacturer = None
+            with open(os.path.join(base_path, device, "manufacturer"), "r") as f:
+                manufacturer = f.readline().strip()
+
+            if manufacturer == vendor_name:
+                with open(os.path.join(base_path, device, "serial"), "r") as f:
+                    serial_number = f.read().strip()
+                tty_path = find_tty_path(device)
+                if tty_path:
+                    for tty in tty_path:
+                        devices.append((serial_number, tty))
+        except IOError:
+            pass  # Ignore files and directories that can't be read
+    return devices
+
+
+def find_tty_path(device):
+    ttys = []
+    tty_base_path = f"/sys/bus/usb/devices/{device}/"
+    for subdir in os.listdir(tty_base_path):
+        try:
+            for subsubdir in os.listdir(os.path.join(tty_base_path, subdir)):
+                if subsubdir.startswith("tty"):
+                    ttys += [
+                        f"/dev/{ttyname}" for ttyname in os.listdir(os.path.join(tty_base_path, subdir, subsubdir))
+                    ]
+        except:
+            pass
+    return ttys
 
 
 def main():
@@ -40,20 +77,28 @@ def main():
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    # read the plist
-    plist = os.popen("ioreg -i -t -r -c AppleUSBACMData -l -a").read()
-    data: List = plistlib.loads(plist.encode())
-    paths = find_subtree_paths(data)
+    serial_and_path = None
+    if sys.platform.startswith("linux"):
+        serial_and_path = find_usb_devices("Espressif")
+        logging.info(f"{serial_and_path = }")
 
-    logging.info("Found {} ESP32 devices".format(len(paths)))
+    if sys.platform.startswith("darwin"):
+        # read the plist
+        plist = os.popen("ioreg -i -t -r -c AppleUSBACMData -l -a").read()
+        data: List = plistlib.loads(plist.encode())
+        paths = find_subtree_paths(data)
 
-    serial_and_path: List[Tuple[str, str]] = [
-        (
-            path[-2].get("USB Serial Number"),
-            path[-1].get("IORegistryEntryChildren")[0].get("IODialinDevice" if args.prefer_tty else "IOCalloutDevice"),
-        )
-        for path in paths
-    ]
+        logging.info("Found {} ESP32 devices".format(len(paths)))
+
+        serial_and_path: List[Tuple[str, str]] = [
+            (
+                path[-2].get("USB Serial Number"),
+                path[-1]
+                .get("IORegistryEntryChildren")[0]
+                .get("IODialinDevice" if args.prefer_tty else "IOCalloutDevice"),
+            )
+            for path in paths
+        ]
 
     if args.serial:
         # find the path for the given serial number
